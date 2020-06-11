996965504a7693fbfbe2d45913751468ef6a36ef
Anwesh43 <anweshthecool0@gmail.com>
2020-06-11 23:58:42 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -39,3 +39,45 @@ export const useDimension = () => {
         }
     })
 }
+
+export const useStyle = (w, h, scale) => {
+    const hSize = Math.min(w, h) / 5
+    const wSize = Math.min(w, h) / 10
+    const position = 'absolute'
+    const sf = sinify(scale)
+    const color = "#2196F3"
+    const fixedColor = "#9E9E9E"
+    const fx = w / 2
+    const fy = h / 2
+    const bars = 5
+    return {
+        getBarStyle(i) {
+            const sfi = divideScale(scale, i, bars)
+            const gap = hSize / bars
+            const left = `${fx - hSize / 2 + i * gap}px`
+            const top = `${fy - wSize * sfi}px`
+            const width = `${gap}px`
+            const height = `${wSize}px`
+            const background = color
+            return {position, left, top, width, height, background}
+        },
+
+        getFixedBar() {
+            const width = `${hSize}px`
+            const height = `${wSize}px`
+            const background = fixedColor
+            const left = `${fx - hSize / 2}px`
+            const top = `${fy - wSize}px`
+            return {position, left, top, width, height, background}
+        },
+
+        getDynamicBar() {
+            const width = `${hSize * sf}px`
+            const height = `${wSize}px`
+            const background = color
+            const left = `${fx - hSize / 2}px`
+            const top = `${fy - wSize}px`
+            return {position, left, top, background, width, height}
+        }
+    }
+}
